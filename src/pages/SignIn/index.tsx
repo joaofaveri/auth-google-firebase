7343e9d43d70f169563a478568af7e003f685fb6
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-09-09 18:48:01 +0000

    feat: signin style

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,18 @@
+import './styles.scss'
+
 export function SignIn() {
   return (
-    <h1>Sign In</h1>
+    <div className="container">
+      <h1>Acesse sua conta</h1>
+
+      <span>
+        Utilizando autenticação social, por exemplo, autenticação com a Google,<br />
+          você facilita a vida do usuário permitindo utilizar sua aplicação <br /> sem fazer cadastro.
+      </span>
+
+      <button type="button" className="button">
+        Entrar com Google
+      </button>
+    </div>
   )
 }
