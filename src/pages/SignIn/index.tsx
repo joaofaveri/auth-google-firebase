b5579a318076397a13b3721ee6c3db8590c6b315
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-09-09 21:11:48 +0000

    feat: update handle google login and show user

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,17 +1,38 @@
-import './styles.scss'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faGoogle} from '@fortawesome/free-brands-svg-icons'
+import { GoogleAuthProvider, signInWithPopup, User } from 'firebase/auth'
+import { useState } from 'react'
+import { auth } from '../../services/firebase'
+import './styles.scss'
 
 export function SignIn() {
+
+  const [user, setUser] = useState<User>({} as User)
+
+  const handleGoogleSignIn = () => {
+    const provider = new GoogleAuthProvider()
+
+    signInWithPopup(auth, provider)
+      .then(result => setUser(result.user))
+      .catch(error => console.log(error))
+  }
+
+
   return (
     <div className="container">
+      <div className='user'>
+        {user.photoURL && <img src={user.photoURL} alt="Foto do Usuário" />}
+        <strong>{user.displayName}</strong>
+        <small>{user.email}</small>
+      </div>
+
       <h1>Acesse sua conta</h1>
 
       <span>
         Utilizando autenticação social como serviço da Google, facilitando a vida do usuário e permitindo utilizar a aplicação sem fazer cadastro.
       </span>
 
-      <button type="button" className="button">
+      <button type="button" className="button" onClick={handleGoogleSignIn}>
         <FontAwesomeIcon icon={faGoogle} />  Entrar com Google
       </button>
     </div>
