11e09609284972f8235deef2f386eac2592dfacf
João Paulo de Fáveri <joao.faveri@gmail.com>
2022-09-09 21:11:14 +0000

    feat: export auth from app firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -9,3 +11,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
